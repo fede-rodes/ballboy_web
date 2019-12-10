2610e40983fabdac0e0a21d6c9248421d5b259de
federodes <federodes@gmail.com>
2019-12-10 13:05:00 +0100

    commenting stories out

--- layouts/BurgerButton/stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import BurgerButton from '.';
-
-storiesOf('layouts.BurgerButton', module)
-  .add('BurgerButton', () => <BurgerButton />);
