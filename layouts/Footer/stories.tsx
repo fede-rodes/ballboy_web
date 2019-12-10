69f608c789262cb03a3ebcdd78c27eee93555e1e
federodes <federodes@gmail.com>
2019-12-10 13:12:14 +0100

    removing stories

--- layouts/Footer/stories.tsx
+++
@@ -1,6 +0,0 @@
-// import React from 'react';
-// import { storiesOf } from '@storybook/react';
-// import Footer from '.';
-
-// storiesOf('layouts.Footer', module)
-//   .add('Footer', () => <Footer />);
--- layouts/Header/stories.tsx
+++
@@ -1,6 +0,0 @@
-// import React from 'react';
-// import { storiesOf } from '@storybook/react';
-// import Header from '.';
-
-// storiesOf('layouts.Header', module)
-//   .add('Header', () => <Header />);
